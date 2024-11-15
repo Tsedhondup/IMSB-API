69d70eca711e0ab47665a145bd9cd29ebaece0e2
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-14 23:53:12 -0500

    seed data created

---
+++ seed_data/inventory_data.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Fiction",
+    PublicationDate: "July 11, 1960",
+    isbn: 9780061120084,
+  },
+
+  {
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian, Science Fiction",
+    publicatioDate: "June 8, 1949",
+    isbn: 9780452284234,
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    genre: "Romance, Fiction",
+    publicationDate: "January 28, 1813",
+    isbn: 9780199535569,
+  },
+];
