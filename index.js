ad9e4551d08846233c38c1d2592cf0b49312c45f
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-16 13:27:09 -0500

    adding book controller created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+app.use(express.json());
+
 // 'dotenv'
 require("dotenv").config();
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
