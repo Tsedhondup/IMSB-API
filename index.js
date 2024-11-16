cf448e4d194f55c28b30d2d61f809ad69c69011f
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-16 10:41:10 -0500

    cors set up completed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+// 'dotenv'
+require("dotenv").config();
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 const PORT = process.env.PORT || 5050;
 const inventoryRoutes = require("./routes/inventory-routes");
 const filterInventoryRoutes = require("./routes/filterInventory-routes");
